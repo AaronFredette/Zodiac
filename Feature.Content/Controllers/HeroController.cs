69a1b9d4436d2e365ca017f8ef15896d77df1fae
Aaron Fredette <aaron.fredette@verndale.com>
2021-12-31 16:37:04 -0500

    adding hero component and ZP site infrastructure

---
+++ Feature.Content/Controllers/HeroController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Constellation.Foundation.ModelMapping;
+using Constellation.Foundation.Mvc;
+using Constellation.Foundation.Mvc.Patterns.Controllers;
+using Feature.Content.Models;
+
+namespace Feature.Content.Controllers
+{
+    public class HeroController : DatasourceRenderingController<HeroModel>
+    {
+        public HeroController(IViewPathResolver viewPathResolver, IModelMapper modelMapper) : base(viewPathResolver,
+            modelMapper)
+        {
+        }
+
+    }
+}
---
+++ Feature.Content/Models/HeroModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using Constellation.Foundation.ModelMapping.FieldModels;
+
+namespace Feature.Content.Models
+{
+    public class HeroModel
+    {
+        public HtmlString HeroDescription { get; set; }
+
+        public ImageModel Image { get; set; }
+
+        public GeneralLinkModel HeroLink { get; set; }
+    }
+}
