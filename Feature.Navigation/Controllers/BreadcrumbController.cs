08381b193184e63edb407ae647e63ae439bb4083
Aaron Fredette <aaron.fredette@verndale.com>
2022-01-04 11:37:28 -0500

    adding new navigation renderings and page templates

---
+++ Feature.Navigation/Controllers/BreadcrumbController.cs
@@ -0,0 +1,30 @@
+﻿using Constellation.Feature.Navigation.Repositories;
+using Constellation.Foundation.Mvc;
+using Sitecore.Data.Items;
+
+namespace Feature.Navigation.Controllers
+{
+    public class BreadcrumbController : ConventionController
+    {
+        public BreadcrumbController(IBreadcrumbNavigationRepository repository, IViewPathResolver viewPathResolver) : base (viewPathResolver)
+        {
+            this.Repository = repository;
+        }
+
+        public IBreadcrumbNavigationRepository Repository { get; set; }
+
+
+        protected override object GetModel(Item datasource, Item contextItem)
+        {
+            return Repository.GetNavigation(contextItem,
+                Sitecore.Context.Site.SiteInfo);
+        }
+
+       
+
+        protected string GetViewName()
+        {
+            return "~/Areas/ZodiacPrime/Views/Navigation/Breadcrumb.cshtml";
+        }
+    }
+}
